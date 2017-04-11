0b55da826727e17c34e86b9293f52d88b49eae03
kmoz <kamil.mozgala@gmail.com>
2017-04-11 16:09:25 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Halcyon-Days
+### Responsive Bootstrap template
+#### [Demo](http://kmoz.github.io/Halcyon-Days)
+#### [PSD Source](https://dribbble.com/shots/1794875-Freebie-PSD-Halcyon-Days-Website-PSD)
---
+++ dist/js/scripts.js
@@ -0,0 +1,23 @@
+
+
+//Scroll animations
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 750);
+        return false;
+      }
+    }
+  });
+});
+
+//counter
+$('.count').counterUp({
+  delay: 10,
+  time: 1000
+});
